54bf8a6b691dad4c8db49e608ad4befb98c9acb2
Kevin Edwards <kedwards@kncedwards.com>
2016-08-17 14:46:11 +0000

    Updated README

--- build/db_init.sql
+++ build/db_init.sql
@@ -5,7 +5,7 @@ CREATE DATABASE todo COLLATE 'utf8mb4_general_ci';
 CREATE USER 'todo_admin'@'localhost' IDENTIFIED BY PASSWORD '*538FE471351B4B8AEB7955E34A4AD980DEFF452D';
 
 -- application db user privileges
-GRANT ALL PRIVILEGES ON todo_.* TO 'todo_admin'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON todo.* TO 'todo_admin'@'localhost' WITH GRANT OPTION;
 
 -- flush
 FLUSH PRIVILEGES;
