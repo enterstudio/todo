7db6f25ca5f86e3fa77b5378534944988df558e9
Kevin Edwards <kedwards@kncedwards.com>
2016-08-17 13:09:29 +0000

    Addd migrations library for easier install

---
+++ app/DoctrineMigrations/Version20160817125926.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160817125926 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(
+            'CREATE TABLE sessions (
+                sess_id varbinary(128) NOT NULL,
+                sess_data blob NOT NULL,
+                sess_time int(10) unsigned NOT NULL,
+                sess_lifetime mediumint(9) NOT NULL,
+                PRIMARY KEY (sess_id)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;'
+        )
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE sessions');
+    }
+}
